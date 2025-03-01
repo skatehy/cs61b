01a30f79fd5d58c4dd4691de45be38fb809f6d98
hybaba <1305685406@qq.com>
2025-03-01 11:49:31 +0800

    finish Java.ecercise

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,7 +6,8 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        int[] array = {1,2,3,4,5,6};
+        return array;
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +16,38 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if(customer.equals("Ergun"))
+        {
+            String[] string = {"beyti", "pizza", "hamburger", "tea"};
+            return string;
+        } else if (customer.equals("Erik")) {
+            String[] string = new String[]{
+                    new String("sushi"),
+                    new String("pasta"),
+                    new String("avocado"),
+                    new String("coffee"),
+            };
+            return string;
+        }
+        else{
+            String[] string = new String[3];
+            return string;
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int min,max;
+        min = array[0];
+        max = array[0];
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]<=min)min = array[i];
+            if(array[i]>=max)max = array[i];
+        }
+        return max-min;
     }
 
     /**
@@ -39,7 +64,20 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if(x == 1)
+        {
+            return list;
+        }
+        else if(x % 2 == 0)
+        {
+            x = x / 2;
+            return hailstoneHelper(x,list);
+        }
+        else {
+            x = x * 3 + 1;
+            return hailstoneHelper(x,list);
+        }
     }
 
 }
